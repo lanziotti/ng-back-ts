ac20fe2c5c223e4315e20adcfc02f3fbfa577553
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-11-16 13:17:19 -0300

    Implementação do endpoint de Saldo do Usuário logado

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,10 @@ import { Request, Response } from 'express';
 import connection from '../database/connection';
 import bcrypt from 'bcrypt';
 
+type AccountId = {
+    accountid: number
+}
+
 class UserController {
     async create(req: Request, res: Response) {
         const { username, passwordUser } = req.body;
@@ -43,8 +47,30 @@ class UserController {
 
 class BalanceUserController {
     async read(req: Request, res: Response) {
-        console.log("BalanceUser")
+        const { accountid } = req.user as AccountId;
+
+        try {
+            const userExists = await connection('users').where({ accountid }).first();
+
+            if (!userExists) {
+                return res.status(400).json({ mensagem: "Esse usuário não existe no banco de dados do sistema." });
+            }
+
+            const balanceExists = await connection('accounts').where({ id: userExists.accountid }).first();
+
+            if (!balanceExists) {
+                return res.status(400).json({ mensagem: "Esse usuário não possui demonstração de saldo." });
+            }
+
+            return res.status(200).json({
+                ...balanceExists,
+                balance: Number(balanceExists.balance)
+            });
+
+        } catch (error) {
+            return res.status(500).json({ mensagem: "Erro interno do servidor." });
+        }
     }
 }
 
-export { UserController, BalanceUserController};
+export { UserController, BalanceUserController };
