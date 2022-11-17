c9ccf2d90502b1d17d85567680de00c96b0c3013
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-11-17 17:59:40 -0300

    Alteração do connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,11 +5,7 @@ const port = process.env.DB_PORT as number | undefined
 const connection = knex({
     client: 'pg',
     connection: {
-        host: process.env.DB_HOST,
-        port: port,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_DATABASE
+        uri: process.env.DB_URI   
     }
 });
 
